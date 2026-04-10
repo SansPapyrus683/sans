c6d739b600ad7295ca10a9c2c36d3f5dd9b62a30
Kevin Sheng <kevinsheng2005@gmail.com>
2026-04-10 08:17:47 -0700

    turns out `dedupe.py` had a TON of bugs

--- twitter/dedupe.py
+++ twitter/dedupe.py
@@ -1,13 +1,29 @@
+import argparse
 import os
-import sys
+import shutil
 from collections import defaultdict
+from pathlib import Path
 
-from PIL import Image
 import imagehash
-
 from parse import Post
+from PIL import Image
 
-os.chdir(sys.argv[1])
+cmd_args = argparse.ArgumentParser(prog="post validator")
+cmd_args.add_argument("directory")
+cmd_args.add_argument(
+    "-r",
+    "--only-recent",
+    action="store_true",
+    help="only check if retweets created today have any duplicates",
+)
+cmd_args.add_argument(
+    "-t",
+    "--test-run",
+    action="store_true",
+    help="copy them into a new directory called dupes to check if they actually are (doesn't delete anything)",
+)
+args = cmd_args.parse_args()
+os.chdir(args.directory)
 
 tweets = defaultdict(lambda: defaultdict(list))
 for i in os.listdir():
@@ -18,13 +34,15 @@ for i in os.listdir():
     hash_ = imagehash.average_hash(Image.open(i))
     tweets[post.author][post.id].append((post, hash_))
 
+
 to_del = []
 duped = []
 for author, g in tweets.items():
     # we wanna removes dupes with the lower id
     ids = sorted(g.keys())
+    # and on each id the images should be in order
     for i in ids:
-        g[i].sort()  # and on each id the images should be in order
+        g[i].sort(key=lambda p: p[0].pos)
 
     for i in range(len(ids)):
         g1 = g[ids[i]]
@@ -35,21 +53,22 @@ for author, g in tweets.items():
                 continue
 
             for a, b in zip(g1, g2):
-                if a[2] - b[2] >= 3:
+                if a[1] - b[1] >= 3:
                     break
             else:
                 to_del.extend(g1)
                 duped.append((g1, g2))
                 break
 
-"""
-for v, (g1, g2) in enumerate(duped):
-    for u, (a, b) in enumerate(zip(g1, g2)):
-        ext1 = os.path.splitext(a[1])[1]
-        ext2 = os.path.splitext(b[1])[1]
-        shutil.copy(a[1], f"dupes/{v}_{u}_0.{ext1}")
-        shutil.copy(b[1], f"dupes/{v}_{u}_1.{ext2}")
-"""
 
-for d in to_del:
-    os.remove(d[1])
+if args.test_run:
+    Path("dupes").mkdir(exist_ok=True)
+    for v, (g1, g2) in enumerate(duped):
+        for u, (a, b) in enumerate(zip(g1, g2)):
+            a_name = str(a[0])
+            b_name = str(b[0])
+            shutil.copy(a_name, f"dupes/{v}_{u}_0.{a[0].ext}")
+            shutil.copy(b_name, f"dupes/{v}_{u}_1.{b[0].ext}")
+else:
+    for d in to_del:
+        os.remove(str(d[0]))
