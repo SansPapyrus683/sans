32620df549e5d59163cdd3378d98394eac6aa5a8
Kevin Sheng <kevinsheng2005@gmail.com>
2026-05-15 20:56:55 -0700

    make a moving over script
    like i have my gallery-dl run on linux and want my photos on the windows
    mount, and this script just looks for all recently created files and
    moves them over to the appropriate location. idt anyone else is gonna
    get any use out of this tbh

---
+++ gallery/move_over.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+    echo "how to use: $0 <from> <to>"
+    exit 1
+fi
+
+from="$1"
+to="$2"
+
+if [[ ! -d $from ]]; then
+    echo "$from isn't a directory"
+    exit 1
+fi
+
+if [[ ! -d $to ]]; then
+    echo "$to isn't a directory"
+    exit 1
+fi
+
+for dir in "$from"/*/; do
+    [ -d "$dir" ] || continue
+
+    dest="$to/$(basename $dir)"
+
+    echo $dir $dest
+
+    find $dir \
+        -type f -cmin -10 \
+        -exec cp -np {} $dest \;
+done
